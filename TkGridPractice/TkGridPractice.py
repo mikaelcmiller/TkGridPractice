60c13c61431d0e76cef1bb9da8305558c817f0bb
MikaelCMiller <mikael.miller@erieri.com>
2017-09-06 15:57:12 -0700

    Working base

--- TkGridPractice/TkGridPractice.py
+++ TkGridPractice/TkGridPractice.py
@@ -4,7 +4,18 @@ from tkinter.ttk import *
 
 class Application(Frame):
 	def __init__(self, master):
+		Frame.__init__(self, master)
+		self.master = master
 		print("initialized")
+		self.grid()
+		self.create_widgets()
+
+	def create_widgets(self):
+		print("creating widgets")
+		self.firstentry = Label(self, text="Mikael")
+		self.firstentry.grid(row=2, column=1, sticky=W)
+		#self.firstentry.pack()
+
 
 root = Tk()
 root.geometry("400x300")
