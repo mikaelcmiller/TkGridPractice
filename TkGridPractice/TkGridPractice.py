d2ac14648e1858f71bb740188e851e243a8cc194
MikaelCMiller <mikael.miller@erieri.com>
2017-09-06 16:28:03 -0700

    2

--- TkGridPractice/TkGridPractice.py
+++ TkGridPractice/TkGridPractice.py
@@ -9,11 +9,17 @@ class Application(Frame):
 		print("initialized")
 		self.grid()
 		self.create_widgets()
+		self.bind_all('<Return>',self.onefunction)
 
 	def create_widgets(self):
 		print("creating widgets")
-		self.firstentry = Label(self, text="Mikael")
+		self.firstentry = Entry(self)
 		self.firstentry.grid(row=2, column=1, sticky=W)
+	
+	def onefunction(self,*event):
+		print("onefunctioncalled")
+		self.firstentry.delete(0, END)
+		self.firstentry.insert(0, "Mikael")
 
 
 root = Tk()
