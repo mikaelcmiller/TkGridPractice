2c38d1e08326156a279fe7566cc8d35830b2f3cd
MikaelCMiller <mikael.miller@erieri.com>
2017-09-08 11:45:04 -0700

    Added clear all method for Entry widgets

--- TkGridPractice/TkGridPractice.py
+++ TkGridPractice/TkGridPractice.py
@@ -11,8 +11,10 @@ class Application(Frame):
 		self.create_widgets()
 		self.bind_all('<Return>',self.resetfunction)
 		self.bind_all('n',self.fetchR1C1)
+		self.bind_all('c',self.clearall)
 
 	def create_widgets(self):
+		self.entrylist = []
 		print("creating widgets")
 		r = range(1,25)
 		for i in r:
@@ -22,6 +24,7 @@ class Application(Frame):
 			self.x = Label(self,text='C'+str(i))
 			self.x.grid(row=0, column=i)
 		self.R1C1 = Entry(self)
+		self.entrylist.append(self.R1C1)
 		self.R1C1.grid(row=1, column=1, sticky=W)
 		self.R2C1 = Label(self, text="R2C1")
 		self.R2C1.grid(row=2, column=1)
@@ -31,11 +34,21 @@ class Application(Frame):
 		self.R3C3.grid(row=3, column=3)
 		#self.R4C4 = Text(self, width=10, height=2) # Text allows users to enter multiple lines of text
 		#self.R4C4.grid(row=4, column=4)
-		self.R5C4 = Entry(self).grid(row=5, column=4) # Use entry for single-line values
+		self.R5C4 = Entry(self)
+		self.R5C4.grid(row=5, column=4) # Use entry for single-line values
 		# self.RC = Label(self, text="RC").grid(row=1, column=0) # widgets stack on top of each other when put in the same grid
-		self.R6C4 = Entry(self).grid(row=6, column=4)
-		self.R20C20 = Entry(self).grid(row=20, column=20)
+		self.entrylist.append(self.R5C4)
+		self.R6C4 = Entry(self)
+		self.R6C4.grid(row=6, column=4)
+		self.entrylist.append(self.R6C4)
+		self.R20C20 = Entry(self)
+		self.R20C20.grid(row=20, column=20)
+		self.entrylist.append(self.R20C20)
 		self.R21C20 = Label(self, text="Label R21C20").grid(row=21, column=20)
+
+	def clearall(self, *event):
+		for label in self.entrylist:
+			label.delete(0, END)
 
 	def resetfunction(self,*event):
 		print("Reset Activated")
